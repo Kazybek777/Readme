d844bb24be9a911fc7908adbbc832f1e08771aea
Kazybek777 <151760232+Kazybek777@users.noreply.github.com>
2026-08-27 13:09:10 +0600

    Week2 oop

--- src/week1/Week1.java
+++ src/week1/Week1.java
@@ -3,19 +3,57 @@ package week1;
 import java.util.Arrays;
 
 public class Week1 {
-    int[] mainMas = {1,2,3,4,5,6,7,8,9,10};
+    static void main() {
 
-    public void Even(){
-        // четные и нечетные
 
-        for (int item : mainMas){
-            if (item % 2 !=  0){
+        Week1 week1 = new Week1();
+        week1.table();
+        week1.rwoNumSum(5);
+        week1.reversInt(321);
+        week1.palindromic("asa");
+        week1.even();
+
+
+
+
+        int age = 20;
+        System.out.println( age > 18 ? "Взрослый " : "дитя");
+
+
+
+        int[] mas1 = {1, 3, 4, 5, 7, 1, 4, 5, 8, 9, 3, 12};
+        int max = 0;
+
+        for (int r : mas1) {
+            if (max < r) {
+                max = r;
+            }
+        }
+        System.out.println("Самое большое число в массиве: " + max);
+
+
+        int[] masSum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int x : masSum) {
+            sum += x;
+        }
+        System.out.println("Сумма всех чисел в массиве " + sum);
+
+
+    }
+
+    int[] mainMas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    public void even() {
+
+        for (int item : mainMas) {
+            if (item % 2 != 0) {
                 System.out.println(item + " в квадрате = " + item * item);
             }
         }
     }
 
-    public void Table() {
+    public void table() {
         for (var i = 1; i <= 9; i++) {
             for (var s = 1; s <= 9; s++) {
 
@@ -26,13 +64,11 @@ public class Week1 {
 
 
         }
-        String arr = "Понравилось. Лежит теперь в любимом. Но дифирамбы петь не буду. Ведь то что я увидел, развидеть я уже не смогу";
-        System.out.println(arr.length());
 
     }
 
 
-    public void TwoNumSum(int s) {
+    public void rwoNumSum(int s) {
 
         int[] nums = {1, 3, 4, 1, 5, 7, 8, 0, 9, 7};
         if (s >= 0) {
@@ -54,7 +90,7 @@ public class Week1 {
 
     }
 
-    public void ReversInt(int number){
+    public void reversInt(int number) {
         int res = 0;
 
         while (number != 0) {
@@ -64,74 +100,13 @@ public class Week1 {
         System.out.println(res);
     }
 
-    public void Palindromic(String text){
-        String s = "";
+    public void palindromic(String text) {
+        String u = "";
 
         for (int i = 0; i < text.length(); i++) {
-            s += text.charAt(text.length() -1 -i);
+            u += text.charAt(text.length() - 1 - i);
 
         }
-        System.out.println(text.equals(s)? true : false);
-
+        System.out.println(text.equals(u));
     }
-
-  /*
-
-    int[] nums = new int[5];
-    for( int i = 0; i < nums.length; i++){
-        nums[i] = i+i;
-    }
-
-    System.out.println(Arrays.toString(nums));
-    var i = (nums.length % 2 == 0) ? true : false;
-    System.out.println(i);
-
-    System.out.println();
-
-
-
-    int age = 20;
-    System.out.println(age < 18 ? true : false);
-
-
-
-
-
-     to sum
-    int[] mass = {1,5,2,1,5,4,8};
-    int e = 0;
-    int s = 10;
-    for (int i = 0; i < mass.length; i++) {
-        for (int j = i+1; j < mass.length; j++) {
-            if (mass[i]  + mass[j] == s){
-                System.out.println(mass[i] + " + " + mass[j] + " = "+ s );
-                e++;
-            }
-        }
-    }
-    System.out.println(e);
-
-
-
-
-    //MAX
-    int [] mas1 = {1,3,4,5,7,1,4,5,8,9,3,12};
-    int max = 0;
-
-    for (int s : mas1){
-        if (max < s){
-            max = s;
-        }
-    }
-    System.out.println(max);
-
-    //all sum in array
-    int[] masSum = { 1,2,3,4,5,6,7,8,9,10};
-    int sum = 0;
-    for (int x : masSum){
-        sum +=x;
-    }
-    System.out.println(sum);
-*/
-
 }
