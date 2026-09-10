71eaee9dab894b58a178ef4b7b3d32e4d8457bf4
Kazybek777 <151760232+Kazybek777@users.noreply.github.com>
2026-09-10 11:49:19 +0600

    finished with sql

---
+++ src/weeks/week4/EmployeeSalaries.sql
@@ -0,0 +1,6 @@
+SELECT name
+from Employee
+where salary > 2000
+  AND months < 10
+
+ORDER BY employee_id;
---
+++ src/weeks/week4/HigherThan75Marks.sql
@@ -0,0 +1,4 @@
+SELECT name
+from students
+where marks > 75
+ORDER BY right(name, 3), id;
